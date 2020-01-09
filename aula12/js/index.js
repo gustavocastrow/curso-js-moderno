d6df18b4d7162f670cc1db143d3c28e6cc90d0c4
Gustavo Castro <gustavocastrocs@gmail.com>
2020-01-09 18:31:56 -0300

    Aula12

---
+++ aula12/js/index.js
@@ -0,0 +1,9 @@
+let num1 = prompt('Digite o primeiro número');
+let num2 = prompt('Digite o segundo número');
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+const soma = num1 + num2;
+
+alert(`A soma de ${num1} + ${num2} é igual à ${soma}`)
