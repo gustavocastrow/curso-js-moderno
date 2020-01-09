b93df053af37e4093a7773a2b2ad49cdaa5dac2f
Gustavo Castro <gustavocastrocs@gmail.com>
2020-01-09 12:26:44 -0300

    Aula10

---
+++ aula10/index.js
@@ -0,0 +1,14 @@
+const nome = 'Luiz' // String
+const nome1 = "Luiz" // String
+const nome2 = `Luiz` // String
+
+const num1 = 10; // Number
+const num2 = 10.54 //Number
+
+let nomeAluno; //undefined -> não aponta para local nenhum na memória
+const sobrenomeAluno = null; // Nulo -> não aponta para local nenhum na memória
+
+const aprovado = false; // Boolean -> true ou false (lógico)
+
+
+console.log(typeof nome); // Verifica o tipo de variável 
