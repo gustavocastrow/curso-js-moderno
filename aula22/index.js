6fb87e134ff094a03975a6c3c0eb16c9d09e99f3
Gustavo Castro <gustavocastrocs@gmail.com>
2020-01-12 15:38:32 -0300

    Aula22

---
+++ aula22/index.js
@@ -0,0 +1,13 @@
+// Operadores Lógicos
+
+// && -> AND (E), todas expressoes precisam ser true para retornar true
+const expressaoAnd = true && true && true;
+console.log(expressaoAnd);
+
+// || -> OR (OU), se uma retorna verdadeiro, ela toda retorna true
+const expressaoOr = true || false;
+console.log(expressaoOr)
+
+//  ! -> NOT (NÃO)
+const expressaoNot = !true;
+console.log(expressaoNot)
