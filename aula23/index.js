71a367efe84b10a16f9a903a249342fb79a3a981
Gustavo Castro <gustavocastrocs@gmail.com>
2020-01-12 17:51:09 -0300

    Aula23

---
+++ aula23/index.js
@@ -0,0 +1,24 @@
+console.log('Gustavo Castro' && 0 && 'Maria');
+
+
+
+// AND 
+function falaOi () {
+  return 'OI';
+}
+
+const vaiExecutar = false;
+
+console.log(vaiExecutar && falaOi());
+
+// OR
+console.log(0 || false || null || 'Gustavo' || true); 
+
+const a = 0;
+const b = null;
+const c = 'false';
+const d = false;
+const e = NaN;
+
+console.log(a || b || c || d || e); //C
+
