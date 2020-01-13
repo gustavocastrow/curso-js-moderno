80b5606bbd4f34c3052574dda99769f577a568c3
Gustavo Castro <gustavocastrocs@gmail.com>
2020-01-13 14:29:40 -0300

    Aula25

---
+++ aula25/index.js
@@ -0,0 +1,11 @@
+const numero = 6;
+
+if (numero >= 0 && numero <= 5){
+  console.log('(A) O número está entre 0 e 5');
+} else if (numero >= 6 && numero <= 8){
+  console.log('(B) O número está entre 6 e 8');
+} else if (numero >= 9 && numero <=11){
+  console.log('(C) O número está entre 9 e 11');
+} else {
+  console.log('(D) O número não está entre 0 e 11');
+}
