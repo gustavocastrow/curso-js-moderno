c3ffcc63f0e82d4a45ca1f4ef39be417b86a2f75
Gustavo Castro <gustavocastrocs@gmail.com>
2020-01-21 06:56:40 -0300

    Aula31

---
+++ aula31/index.js
@@ -0,0 +1,7 @@
+const numeros = [563, 3242, 3643, 12412, 54523, 6532, 71234, 5238 ,3229];
+const [primeiroNumero, segundoNumero, ...resto] = numeros;
+
+console.log(primeiroNumero, segundoNumero, resto);
+
+
+
