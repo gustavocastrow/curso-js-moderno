83162db68d38bed3213a54e42fed35fa4f9009ad
Gustavo Castro <gustavocastrocs@gmail.com>
2020-01-22 05:59:43 -0300

    Aula33

---
+++ aula33/index.js
@@ -0,0 +1,12 @@
+for (let i = 0 ; i <= 10; i ++) {
+  const par = i % 2 === 0 ? 'par' : 'impar';
+  console.log(i, par);
+
+}
+
+
+const frutas = ['Maça', 'Pêra', 'Uva'];
+
+for (let i = 0; i <frutas.length; i++){
+  console.log(`Indice ${i}`, frutas[i]);
+}
