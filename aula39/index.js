117690b363fe9b7d89b260f4f57d8585a32ef6c1
Gustavo Castro <gustavocastrocs@gmail.com>
2020-01-23 04:32:57 -0300

    Aula39

---
+++ aula39/index.js
@@ -0,0 +1,14 @@
+//Escreva um função que recebe dois números e retorna o maior deles
+
+
+function max (x, y) {
+  if (x > y){
+    return x;
+  } else {
+    return y;
+  }
+}
+
+console.log(max(3,1));
+
+
